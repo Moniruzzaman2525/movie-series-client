3f7843f55623afa042bf8db47c14312bc93c92df
Rabeya Akter <rabeyariya251@gmail.com>
2025-04-30 08:31:45 +0600

    reslove hyfration error

--- src/app/(withNavFooter)/layout.tsx
+++ src/app/(withNavFooter)/layout.tsx
@@ -1,10 +1,18 @@
+import Footer from "@/components/Shared/Footer/Footer";
+import Navbar from "@/components/Shared/Navbar/Navbar";
 import React, { ReactNode } from "react";
 interface MainLayoutProps {
   children: ReactNode;
 }
 
 const MainLayout = ({ children }: MainLayoutProps) => {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <Navbar />
+      {children}
+      <Footer />
+    </div>
+  );
 };
 
 export default MainLayout;
---
+++ src/components/Shared/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+          <Button>Click me</Button>
+      <h1>Navbar</h1>
+    </div>
+  );
+};
+
+export default Navbar;
