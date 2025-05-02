9dc01634e9e483490983cf3dbd7b6cc42e0577c1
Rabeya Akter <rabeyariya251@gmail.com>
2025-05-02 16:27:57 +0600

    try to fix the marquee components

--- src/components/HomeCompoents/Marquee.tsx
+++ src/components/HomeCompoents/Marquee.tsx
@@ -2,43 +2,18 @@
 
 import { ThreeDMarquee } from "../ui/3d-marquww";
 
-
 export function ThreeDMarqueeDemo() {
   const images = [
-    "https://assets.aceternity.com/cloudinary_bkp/3d-card.png",
-    "https://assets.aceternity.com/animated-modal.png",
-    "https://assets.aceternity.com/animated-testimonials.webp",
+    "https://i.ibb.co/ycxWDDPW/jordan-tan-Ez1-PYe-J35w-E-unsplash.jpg",
+    "https://i.ibb.co/XrBS1t9r/shruti-deo-9ya6-PUGNl-GI-unsplash.jpg",
+    "https://i.ibb.co/p6JZXw3/olena-babinets-83-RTVTFo-Orw-unsplash.jpg",
     "https://assets.aceternity.com/cloudinary_bkp/Tooltip_luwy44.png",
-    "https://assets.aceternity.com/github-globe.png",
-    "https://assets.aceternity.com/glare-card.png",
-    "https://assets.aceternity.com/layout-grid.png",
-    "https://assets.aceternity.com/flip-text.png",
-    "https://assets.aceternity.com/hero-highlight.png",
-    "https://assets.aceternity.com/carousel.webp",
-    "https://assets.aceternity.com/placeholders-and-vanish-input.png",
-    "https://assets.aceternity.com/shooting-stars-and-stars-background.png",
-    "https://assets.aceternity.com/signup-form.png",
-    "https://assets.aceternity.com/cloudinary_bkp/stars_sxle3d.png",
-    "https://assets.aceternity.com/spotlight-new.webp",
-    "https://assets.aceternity.com/cloudinary_bkp/Spotlight_ar5jpr.png",
-    "https://assets.aceternity.com/cloudinary_bkp/Parallax_Scroll_pzlatw_anfkh7.png",
-    "https://assets.aceternity.com/tabs.png",
-    "https://assets.aceternity.com/cloudinary_bkp/Tracing_Beam_npujte.png",
-    "https://assets.aceternity.com/cloudinary_bkp/typewriter-effect.png",
-    "https://assets.aceternity.com/glowing-effect.webp",
-    "https://assets.aceternity.com/hover-border-gradient.png",
-    "https://assets.aceternity.com/cloudinary_bkp/Infinite_Moving_Cards_evhzur.png",
-    "https://assets.aceternity.com/cloudinary_bkp/Lamp_hlq3ln.png",
-    "https://assets.aceternity.com/macbook-scroll.png",
-    "https://assets.aceternity.com/cloudinary_bkp/Meteors_fye3ys.png",
-    "https://assets.aceternity.com/cloudinary_bkp/Moving_Border_yn78lv.png",
-    "https://assets.aceternity.com/multi-step-loader.png",
-    "https://assets.aceternity.com/vortex.png",
-    "https://assets.aceternity.com/wobble-card.png",
-    "https://assets.aceternity.com/world-map.webp",
+    "https://i.ibb.co/PZxw3ff/dat-nguyen-c-IN4-rto-UIM-unsplash.jpg",
+    "https://i.ibb.co/6Jvz0qPC/mike-yukhtenko-wfh8d-Dl-NFOk-unsplash.jpg",
   ];
+
   return (
-    <div className="mx-auto my-10 max-w-7xl rounded-3xl bg-gray-950/5 p-2 ring-1 ring-neutral-700/10 dark:bg-neutral-800">
+    <div className="w-full   rounded-3xl bg-gray-950/5 ring-1 ring-neutral-700/10 dark:bg-neutral-800">
       <ThreeDMarquee images={images} />
     </div>
   );
